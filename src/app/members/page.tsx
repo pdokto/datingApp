775883be72fb3d396029430ff141206cee92a824
Patrick Doktorowski <p.doktorowski@googlemail.com>
2025-03-18 19:50:48 +0100

    basic routing

---
+++ src/app/members/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function MembersPage() {
+  return (
+    <>
+        <h1>MembersPage</h1>
+        <Link href='/'>back</Link>
+    </>
+  )
+}
